6c6401005673dd0c5298a8569f881439313e9a00
onmyoji-xiao <46000350+onmyoji-xiao@users.noreply.github.com>
2025-11-10 16:26:24 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,100 @@
+import os
+
+import torch
+from random import randint
+from omegaconf import OmegaConf
+import sys
+from argparse import ArgumentParser, Namespace
+import pytorch_lightning as pl
+import numpy as np
+import random
+from dataset.dataset import ReplicaDataset, ScannetDataset
+from dataset.datamodule import DataModule
+from pytorch_lightning import Trainer, loggers
+from pytorch_lightning.callbacks import (
+    LearningRateMonitor,
+    ModelCheckpoint,
+)
+from models.model_wrapper import ModelWrapper
+import time
+
+
+def train(args):
+    # This allows the current step to be shared with the data loader processes.
+    # step_tracker = StepTracker()
+    cfg = OmegaConf.load(args.cfg)
+    print(cfg)
+    callbacks = []
+    callbacks.append(
+        ModelCheckpoint(
+            os.path.join(args.output_dir, "checkpoints"),
+            filename="ckpt_step-{step:06d}",
+            auto_insert_metric_name=False,
+            monitor="val/miou",
+            save_top_k=5,
+            mode="max"
+        )
+    )
+
+    date = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
+
+    if args.mode == 'test':
+        logger = None
+    else:
+        logger = loggers.TensorBoardLogger("tb_logs", name=f'{args.expname}', version=f'{date}')
+
+    trainer = Trainer(
+        max_epochs=-1,
+        accelerator="gpu",
+        logger=logger,
+        devices=[6],
+        strategy="ddp_find_unused_parameters_true" if torch.cuda.device_count() > 1 else "auto",
+        callbacks=callbacks,
+        val_check_interval=cfg.trainer.val_check_interval,
+        gradient_clip_val=cfg.trainer.gradient_clip_val,
+        max_steps=cfg.trainer.max_steps,
+        num_sanity_val_steps=cfg.trainer.num_sanity_val_steps,
+    )
+
+    model_wrapper = ModelWrapper(
+        cfg,
+        args,
+    )
+    data_module = DataModule(
+        ReplicaDataset if "replica" in args.cfg else ScannetDataset,
+        OmegaConf.merge(cfg.base, cfg.data),
+        global_rank=trainer.global_rank,
+    )
+
+    if args.mode == 'train':
+        trainer.fit(model_wrapper, datamodule=data_module, ckpt_path=args.ckpt if args.ckpt != "" else None)
+    else:
+        trainer.validate(model_wrapper, datamodule=data_module, ckpt_path=args.ckpt)
+
+
+def seed_everything(seed=1234):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Training script parameters")
+    parser.add_argument('--dataset_name', type=str, default="replica")
+    parser.add_argument('--cfg', type=str, default="./configs/replica_seg.yaml")
+    parser.add_argument('--mode', type=str, default="train")
+    parser.add_argument('--save_dir', type=str, default="./save")
+    parser.add_argument('--ckpt', type=str, default="")
+    parser.add_argument('--expname', type=str, default="base")
+    parser.add_argument("--ddp", action="store_true", help="Use distributed data parallel")
+    parser.add_argument("--seed", type=int, default=2024, help="Random seed")
+
+    args = parser.parse_args()
+    args.output_dir = os.path.join(args.save_dir, args.expname)
+
+    ## Setting seeds
+    seed_everything(args.seed)
+    pl.seed_everything(args.seed)
+
+    train(args)
---
+++ utils/utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+lable_color_map = np.array([
+    [174, 199, 232],  # wall
+    [152, 223, 138],  # floor
+    [31, 119, 180],   # cabinet
+    [255, 187, 120],  # bed
+    [188, 189, 34],   # chair
+    [140, 86, 75],    # sofa
+    [255, 152, 150],  # table
+    [214, 39, 40],    # door
+    [197, 176, 213],  # window
+    [148, 103, 189],  # bookshelf
+    [196, 156, 148],  # picture
+    [23, 190, 207],   # counter
+    [247, 182, 210],  # desk
+    [219, 219, 141],  # curtain
+    [255, 127, 14],   # refrigerator
+    [91, 163, 138],   # shower curtain
+    [44, 160, 44],    # toilet
+    [112, 128, 144],  # sink
+    [227, 119, 194],  # bathtub
+    [82, 84, 163],    # otherfurn
+    [248, 166, 116],  # invalid
+    ])
